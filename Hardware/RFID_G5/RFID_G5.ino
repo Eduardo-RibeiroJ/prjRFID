ab0586f0f9f9f239e4f901979dca33b8ad15e24c
AdilsonJR6 <adilsondiasjunior@live.com>
2019-06-06 21:59:06 -0300

    Alterações no código e finalizar documentação

--- Hardware/RFID_G5/RFID_G5.ino
+++ Hardware/RFID_G5/RFID_G5.ino
@@ -53,15 +53,24 @@ MySQL_Connection conn((Client *)&client);
  
 void setup() 
 { 
+   /**
+    * Inicia Monitor Serial, SPI, RFID e Ethernet
+    */
    Serial.begin(115200);
    SPI.begin();  
    mfrc522.PCD_Init();
-   
    while (!Serial); 
    Ethernet.begin(mac_addr);
-   Serial.println("Conectando...");
-    
-   while(!conectado){
+   Conectar();
+}
+
+/**
+ * Função para fazer a conexão com o banco de dados MYSQL
+ * Essa função só é retornada quando a conexão é estabelecida
+ */
+bool Conectar(){
+  Serial.println("Conectando...");
+  while(!conectado){
       conectado = conn.connect(server_addr, 41890, user, password);
       if(conectado){
         Serial.println("Conectado");
@@ -70,45 +79,53 @@ void setup()
         MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
         cur_mem->execute(BANCODEDADOS);
         delete cur_mem;
+
+        return true;
       }
       else{
         Serial.println("A conexão falhou");
         conn.close();
       }
    }
-   
 }
- 
  
 void loop() 
 {
-    if ( ! mfrc522.PICC_IsNewCardPresent()) 
-    {
-      return;
-    }
-    if ( ! mfrc522.PICC_ReadCardSerial()) 
-    {
-      return;
-    }
+  /**
+   * Verifica se o leitor está operando normalmente e se tem algum cartão presente
+   * Caso não tenha a função é retornada e começa novamente
+   */
+  if(!mfrc522.PICC_IsNewCardPresent()){
+    return;
+  }
+  if(!mfrc522.PICC_ReadCardSerial()){
+    return;
+  }
 
-    String conteudo= "";
-    for (byte i = 0; i < mfrc522.uid.size; i++) 
-    {=
-       conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
-    }
-    Serial.println(conteudo);
+  /**
+   * O UID do cartão é gravado na String conteudo e é transfromada em char
+   * para poder utilizar a função sprintf que insere o uid do cartão
+   * na query INSERIR_CARTAO que é declarada no inicio do código 
+   */
+  String conteudo= "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {=
+     conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
 
-    int str_len = conteudo.length() + 1; 
-    char uid_cartao[str_len];
-    conteudo.toCharArray(uid_cartao, str_len);
-  
-   Serial.println("Executando sentença");
-   sprintf(sentenca, INSERIR_CARTAO, uid_cartao);
-   
-   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-   cur_mem->execute(sentenca);
-   Serial.println(sentenca);
-   delete cur_mem;
- 
-   delay(100);
+  int str_len = conteudo.length() + 1; 
+  char uid_cartao[str_len];
+  conteudo.toCharArray(uid_cartao, str_len);
+  sprintf(sentenca, INSERIR_CARTAO, uid_cartao);
+
+  /**
+   * Execução da Query no banco de dados inserindo o UID do cartão
+   */
+  Serial.print("Executando Query:");
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+  cur_mem->execute(sentenca);
+  Serial.println(sentenca);
+  delete cur_mem;
+
+  delay(100);
 }
