464b2c8f0f2df1c1a3f2e7c00ee501f75875c1bd
Evandro de Souza <evandro.souza@melhorescola.net>
2019-08-10 11:44:14 -0300

    json produtos

--- dadosJson.php
+++ dadosJson.php
@@ -5,21 +5,29 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 /* DADOS JSON DO CONTRATO */
 
+include_once "Controller/ProdutoController.php";
+
 include_once "Controller/MovimentacaoController.php";
 
 include_once "Controller/EtiquetaController.php";
 
 
 
+//LISTA DE TODOS OS PRODUTOS
+//exemplo de url http://127.0.0.1/prjRFID/dadosJson.php?acao=produtos&id=1
+if(isset($_GET['acao']) && $_GET['acao'] == 'produtos'){
+	echo ProdutoController::getProdutosJson();
+}
+
 //LISTA DE PRODUTOS EM UM CONTRATO
-//exemplo de url http://127.0.0.1/prjRFID/dadosProdutos.php?idContrato=45612&status=S
+//exemplo de url http://127.0.0.1/prjRFID/dadosJson.php?idContrato=45612&status=S
 if(isset($_GET['idContrato']) && isset($_GET['status'])){
 
 	echo MovimentacaoController::getProdutosByMovimentacao($_GET['idContrato'], true);
 }
 
 //TEMPORARIAS
-//exemplo de url http://127.0.0.1/prjRFID/dadosProdutos.php?acao=temp
+//exemplo de url http://127.0.0.1/prjRFID/dadosJson.php?acao=temp
 if(isset($_GET['acao']) && ($_GET['acao']=='temp')){
 	
 	echo MovimentacaoController::getTemp(true);
@@ -34,7 +42,7 @@ if(isset($_GET['acao']) && ($_GET['acao']=='tempContrato')){
 
 
 //TEMPORARIAS
-//exemplo de url http://127.0.0.1/prjRFID/dadosProdutos.php?acao=temp
+//exemplo de url http://127.0.0.1/prjRFID/dadosJson.php?acao=temp
 if(isset($_GET['acao']) && ($_GET['acao']=='tempetiquetas')){
 	
 	echo MovimentacaoController::getTempEtiquetas(true);
@@ -43,7 +51,7 @@ if(isset($_GET['acao']) && ($_GET['acao']=='tempetiquetas')){
 
 
 //JA VINCULADAS A PRODUTOS
-//exemplo de url http://127.0.0.1/prjRFID/dadosProdutos.php?acao=etiquetas
+//exemplo de url http://127.0.0.1/prjRFID/dadosJson.php?acao=etiquetas
 if(isset($_GET['acao']) && ($_GET['acao']=='etiquetas')){
 	
 	echo EtiquetaController::getEtiquetas(0, true);
